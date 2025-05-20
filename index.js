6ffb17627e9baf38945a98fd766d35fd9797a926
Ryan Fiset <ryfiset@gmail.com>
2025-05-19 19:05:12 -0700

    Complete base game logic

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
 function setup() {
   let firstCard = undefined;
   let secondCard = undefined;
-  $(".card").on("click", function () {
-    $(this).toggleClass("flip");
+  let isProcessing = false;
 
-    if (!firstCard) firstCard = $(this).find(".front_face")[0];
-    else {
-      secondCard = $(this).find(".front_face")[0];
-      console.log(firstCard, secondCard);
-      if (firstCard.src == secondCard.src) {
-        console.log("match");
-        $(`#${firstCard.id}`).parent().off("click");
-        $(`#${secondCard.id}`).parent().off("click");
-      } else {
-        console.log("no match");
-        setTimeout(() => {
-          $(`#${firstCard.id}`).parent().toggleClass("flip");
-          $(`#${secondCard.id}`).parent().toggleClass("flip");
-        }, 1000);
-      }
+  $(".card").on("click", function () {
+    const card = $(this);
+    // If the card is already flipped or processing is happening do nothing
+    if (isProcessing || isFlipped(card)) return;
+
+    // Flip the clicked card
+    flip(card);
+
+    if (!firstCard) {
+      firstCard = card;
+    } else {
+      secondCard = card;
+      isProcessing = true;
+
+      run(firstCard, secondCard);
+
+      // Reset state after 1 second
+      setTimeout(() => {
+        firstCard = undefined;
+        secondCard = undefined;
+        isProcessing = false;
+      }, 1000);
     }
   });
+}
+
+function run(firstCard, secondCard) {
+  if (cardsMatch(firstCard, secondCard)) {
+    console.log("match");
+    firstCard.off("click");
+    secondCard.off("click");
+  } else {
+    console.log("no match");
+
+    // Flip cards after 1 second
+    setTimeout(() => {
+      flip(firstCard);
+      flip(secondCard);
+    }, 1000);
+  }
+}
+
+function isFlipped(card) {
+  return card.hasClass("flip");
+}
+
+function flip(card) {
+  card.toggleClass("flip");
+}
+
+function cardsMatch(firstCard, secondCard) {
+  const firstCardImg = firstCard.find(".front_face")[0];
+  const secondCardImg = secondCard.find(".front_face")[0];
+
+  return (
+    firstCardImg.id !== secondCardImg.id &&
+    firstCardImg.src === secondCardImg.src
+  );
 }
 
 $(document).ready(setup);
