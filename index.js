ff8b0600ae6f61e826154d37501cd9ae6966bdae
Ryan Fiset <ryfiset@gmail.com>
2025-05-19 20:44:34 -0700

    Pull pokemon from API and dynamically load them

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+const TOTAL_POKEMON = 1025;
+
+async function fetchRandomPokemon() {
+  const numPairs = 3;
+
+  const ids = new Set();
+  while (ids.size < numPairs) {
+    ids.add(Math.floor(Math.random() * TOTAL_POKEMON) + 1);
+  }
+
+  const promises = [...ids].map((id) =>
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => res.json())
+  );
+
+  return Promise.all(promises);
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function createCard(pokemon) {
+  return `
+    <div class="card" data-id="${pokemon.id}">
+      <img class="front_face" src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}">
+      <img class="back_face" src="back.webp" alt="pokeball"> 
+    </div>
+  `;
+}
+
+async function buildGameBoard() {
+  const pokemon = await fetchRandomPokemon();
+
+  // Duplicate for pairs
+  const paired = [...pokemon, ...pokemon];
+
+  shuffle(paired);
+
+  const html = paired.map((pokemon) => createCard(pokemon)).join("");
+  $("#game_grid").html(html);
+
+  setup();
+}
+
 function setup() {
   let firstCard = undefined;
   let secondCard = undefined;
@@ -5,6 +52,7 @@ function setup() {
 
   $(".card").on("click", function () {
     const card = $(this);
+
     // If the card is already flipped or processing is happening do nothing
     if (isProcessing || isFlipped(card)) return;
 
@@ -34,15 +82,14 @@ function run(firstCard, secondCard) {
     console.log("match");
     firstCard.off("click");
     secondCard.off("click");
-  } else {
-    console.log("no match");
-
-    // Flip cards after 1 second
-    setTimeout(() => {
-      flip(firstCard);
-      flip(secondCard);
-    }, 1000);
+    return;
   }
+  console.log("no match");
+  // Flip cards after 1 second
+  setTimeout(() => {
+    flip(firstCard);
+    flip(secondCard);
+  }, 1000);
 }
 
 function isFlipped(card) {
@@ -54,13 +101,10 @@ function flip(card) {
 }
 
 function cardsMatch(firstCard, secondCard) {
-  const firstCardImg = firstCard.find(".front_face")[0];
-  const secondCardImg = secondCard.find(".front_face")[0];
-
   return (
-    firstCardImg.id !== secondCardImg.id &&
-    firstCardImg.src === secondCardImg.src
+    firstCard.data("id") === secondCard.data("id") &&
+    firstCard[0] !== secondCard[0]
   );
 }
 
-$(document).ready(setup);
+$(document).ready(buildGameBoard);
